ce4f56bc3004b266046a6e8b007b1266e9594d64
Kannan <22211670+g-kannan@users.noreply.github.com>
2023-09-18 23:56:56 +0000

    PersonalPortfolioV1

--- Hello.py
+++ Hello.py
@@ -19,32 +19,42 @@ LOGGER = get_logger(__name__)
 
 
 def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+  st.set_page_config(page_title='Kannan G\'s portfolio' ,layout="wide")
 
-    st.write("# Welcome to Streamlit! 👋")
+  st.sidebar.caption('Wish to connect?')
+  # st.sidebar.write('Mail: ')
+  st.sidebar.write('[LinkedIn](https://www.linkedin.com/in/kannan-g-212242111)')
 
-    st.sidebar.success("Select a demo above.")
+  st.subheader('Summary')
+  st.write("Hi, I'm Kannan. Working as Data Architect @ [Ganit](https://www.ganitinc.com/) with 12+ years of experience in helping business to use thier Data as competitive advantage")
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+  st.divider()
+  st.subheader('Skills')
+  col1, col2, col3 = st.columns(3)
+
+  with col1:
+    st.markdown("###### Python")
+    st.markdown("###### SQL")
+    st.markdown("###### Spark")
+    st.markdown("###### Docker")
+
+  with col2:
+    st.markdown("###### Databricks")
+    st.markdown("###### Snowflake")
+
+  with col3:
+    st.markdown("###### Azure Data Factory")
+    st.markdown("###### Azure Synapse")
+    st.markdown("###### AWS Glue")
+
+  st.divider()
+  st.subheader('Certifications')
+  DEALink='##### Databricks Certified: [Data Engineer Associate](https://scq.io/rqVTqa2)'
+  GCALink="##### Google Cloud Certified: [Associate Cloud Engineer](https://www.credential.net/55114231-b1b0-4430-b602-ea78deae5c1d?key=b079ea0eb808fde9a09e9da16ed5d60d7db415926aeb1efcc6e3a6cf76a6a63f)"
+  AZDBALink="##### Microsoft Certified: [Azure Database Administrator Associate](https://www.credly.com/badges/5a75c0de-a9b4-49e3-9bba-b26671c16ab0)"
+  st.markdown(DEALink,unsafe_allow_html=True)
+  st.markdown(GCALink,unsafe_allow_html=True)
+  st.markdown(AZDBALink,unsafe_allow_html=True)
 
 
 if __name__ == "__main__":
